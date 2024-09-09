cbe254f4596fb5a01bf5c146a16ac1747dee4715
FRIDAUS <ayobamifridaus@gmail.com>
2024-09-09 11:24:42 +0100

    update

--- api/build.sh
+++ api/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-
+apt-get update && apt-get install -y \
+  libcairo2 \
+  libcairo2-dev \
+  pkg-config \
+  python3-dev
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
